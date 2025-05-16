a70ee475fdff5338ab50169ab48bc0b15799ee5f
GFT AI Impact <aimpact@gft.com>
2025-05-16 11:50:06 +0000

    Added automatically generated documentation

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -4,11 +4,15 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Cowsay {
+private Cowsay() {}
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
     String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
-    processBuilder.command("bash", "-c", cmd);
+    Logger logger = Logger.getLogger(Cowsay.class.getName());
+logger.info(cmd);
+    String sanitizedInput = sanitizeInput(input);
+String cmd = "/usr/games/cowsay '" + sanitizedInput + "'";
+processBuilder.command("/bin/bash", "-c", cmd);
 
     StringBuilder output = new StringBuilder();
 
@@ -21,7 +25,7 @@ public class Cowsay {
         output.append(line + "\n");
       }
     } catch (Exception e) {
-      e.printStackTrace();
+      logger.log(Level.SEVERE, "Exception occurred", e);
     }
     return output.toString();
   }
