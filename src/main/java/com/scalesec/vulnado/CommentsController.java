a8e44562ffd96c9ec6ada47b5a0bda6ec9c7a773
GFT AI Impact <aimpact@gft.com>
2025-06-27 10:00:40 +0000

    Added automatically generated documentation

--- src/main/java/com/scalesec/vulnado/CommentsController.java
+++ src/main/java/com/scalesec/vulnado/CommentsController.java
@@ -5,37 +5,65 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.autoconfigure.*;
 import java.util.List;
+import java.util.logging.Logger;
 import java.io.Serializable;
 
 @RestController
 @EnableAutoConfiguration
 public class CommentsController {
   @Value("${app.secret}")
+private static final Logger LOGGER = Logger.getLogger(CommentsController.class.getName());
   private String secret;
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/comments", method = RequestMethod.GET, produces = "application/json")
+  @CrossOrigin(origins = \"http://trusted-domain.com\")
+  @GetMapping(value = \"/comments\", produces = \"application/json\")
+LOGGER.info(\"Comment fetch finalized.\");
+LOGGER.info(\"Comment fetch initiated.\");
+LOGGER.info(\"Processing comment fetch request.\");
   List<Comment> comments(@RequestHeader(value="x-auth-token") String token) {
+LOGGER.info(\"User authentication finalized.\");
+LOGGER.info(\"User authentication initiated.\");
+LOGGER.info(\"User authenticated successfully.\");
+LOGGER.info(\"Authenticating user token.\");
     User.assertAuth(secret, token);
+LOGGER.info(\"Comment fetch finalized.\");
+LOGGER.info(\"Comment fetch completed.\");
+LOGGER.info(\"Fetching all comments.\");
     return Comment.fetch_all();
   }
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/comments", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+  @CrossOrigin(origins = \"http://trusted-domain.com\")
+  @PostMapping(value = \"/comments\", produces = \"application/json\", consumes = \"application/json\")
+LOGGER.info(\"Comment creation finalized.\");
+LOGGER.info(\"Comment creation initiated.\");
+LOGGER.info(\"Processing comment creation request.\");
   Comment createComment(@RequestHeader(value="x-auth-token") String token, @RequestBody CommentRequest input) {
+LOGGER.info(\"Comment creation finalized.\");
+LOGGER.info(\"Comment creation completed.\");
+LOGGER.info(\"Creating a new comment.\");
     return Comment.create(input.username, input.body);
+LOGGER.info(\"Comment creation finalized.\");
+LOGGER.info(\"Comment created successfully.\");
   }
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/comments/{id}", method = RequestMethod.DELETE, produces = "application/json")
+  @CrossOrigin(origins = \"http://trusted-domain.com\")
+  @DeleteMapping(value = \"/comments/{id}\", produces = \"application/json\")
+LOGGER.info(\"Comment deletion finalized.\");
+LOGGER.info(\"Comment deletion initiated.\");
+LOGGER.info(\"Processing comment deletion request.\");
   Boolean deleteComment(@RequestHeader(value="x-auth-token") String token, @PathVariable("id") String id) {
+LOGGER.info(\"Comment deletion finalized.\");
+LOGGER.info(\"Comment deletion completed.\");
+LOGGER.info(\"Deleting a comment.\");
     return Comment.delete(id);
+LOGGER.info(\"Comment deletion finalized.\");
+LOGGER.info(\"Comment deleted successfully.\");
   }
 }
 
 class CommentRequest implements Serializable {
-  public String username;
-  public String body;
+  private String username;
+  private String body;
 }
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
