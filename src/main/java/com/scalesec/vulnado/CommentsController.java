b02d2dbc1f20994997d11a066ef0defc1340eb49
GFT AI Impact <aimpact@gft.com>
2025-09-22 10:19:16 +0000

    Added automatically generated documentation

--- src/main/java/com/scalesec/vulnado/CommentsController.java
+++ src/main/java/com/scalesec/vulnado/CommentsController.java
@@ -5,48 +5,65 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.autoconfigure.*;
 import java.util.List;
+import java.util.logging.Logger;
 import java.io.Serializable;
 
+LOGGER.info(\"CommentsController initialized.\");
 @RestController
 @EnableAutoConfiguration
+LOGGER.info(\"AutoConfiguration enabled.\");
+private static final Logger LOGGER = Logger.getLogger(CommentsController.class.getName());
 public class CommentsController {
   @Value("${app.secret}")
+LOGGER.info(\"Secret value initialized.\");
   private String secret;
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/comments", method = RequestMethod.GET, produces = "application/json")
+  @CrossOrigin(origins = \"http://example.com\")
+  @GetMapping(value = \"/comments\", produces = \"application/json\")
   List<Comment> comments(@RequestHeader(value="x-auth-token") String token) {
+LOGGER.info(\"Authenticating user token.\");
     User.assertAuth(secret, token);
     return Comment.fetch_all();
   }
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/comments", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+  @CrossOrigin(origins = \"http://example.com\")
+  @PostMapping(value = \"/comments\", produces = \"application/json\", consumes = \"application/json\")
   Comment createComment(@RequestHeader(value="x-auth-token") String token, @RequestBody CommentRequest input) {
+LOGGER.info(\"Creating a new comment.\");
     return Comment.create(input.username, input.body);
   }
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/comments/{id}", method = RequestMethod.DELETE, produces = "application/json")
+  @CrossOrigin(origins = \"http://example.com\")
+  @DeleteMapping(value = \"/comments/{id}\", produces = \"application/json\")
   Boolean deleteComment(@RequestHeader(value="x-auth-token") String token, @PathVariable("id") String id) {
+LOGGER.info(\"Deleting a comment.\");
     return Comment.delete(id);
   }
 }
 
+LOGGER.info(\"Serializable interface implemented.\");
+LOGGER.info(\"CommentRequest class initialized.\");
 class CommentRequest implements Serializable {
-  public String username;
-  public String body;
+  private String username;
+  private String body;
 }
 
+LOGGER.info(\"HttpStatus.BAD_REQUEST annotation applied.\");
+LOGGER.info(\"BadRequest class initialized.\");
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 class BadRequest extends RuntimeException {
+LOGGER.warning(\"Bad request encountered.\");
   public BadRequest(String exception) {
     super(exception);
   }
 }
 
+LOGGER.info(\"ServerError class initialized
+LOGGER.info(\"HttpStatus.INTERNAL_SERVER_ERROR annotation applied.\");
+LOGGER.info(\"ServerError class initialized.\");
 @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 class ServerError extends RuntimeException {
+LOGGER.severe(\"Internal server error encountered.\");
   public ServerError(String exception) {
     super(exception);
   }
